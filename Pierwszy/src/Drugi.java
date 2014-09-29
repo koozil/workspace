308e5c272c50f55b71c443508c14822d54f84690
Filip Kozłowski <kooziloo@gmail.com>
2014-09-29 12:58:53 +0200

    initial#2

---
+++ Pierwszy/src/Drugi.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Drugi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("podaj swoje imiê: ");
+	String imie;
+	Scanner odczyt = new Scanner(System.in);
+	imie = odczyt.nextLine();
+	System.out.println("Witaj, " +imie);
+	}
+}
+
---
+++ Pierwszy/src/Pierwszy.java
@@ -0,0 +1,11 @@
+
+public class Pierwszy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println ("zaczynamy programowanie w d¿awie");
+System.out.println ("OMG JA PROGRAMUJE W JAVIE <3 LOFFCIAM");
+
+	}
+
+}
