d303757f661cc63c2426e468a32864e29676a624
Filip Kozłowski <kooziloo@gmail.com>
2014-10-01 13:49:23 +0200

    ZADANKA. 1/9

---
+++ ParzysteNieparzyste/src/Zadanko.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Zadanko {
+	private static Scanner odczyt;
+
+	static Double a, b;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Podaj pierwszš zmiennš: ");
+		odczyt = new Scanner(System.in);
+		a = odczyt.nextDouble();
+		System.out.println("Podaj druga zmiennš: ");
+		b = odczyt.nextDouble();
+		System.out.println("Wynik dodawania: " + (a + b));
+		System.out.println("Wynik odejmowania: " + (a - b));
+		System.out.println("Wynik mno¿enia: " + (a * b));
+		System.out.println("Wynik dzielenia: " + (a / b));
+	}
+
+}
