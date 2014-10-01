3dc58d6a88760c6fe95c5762b4746885e6be53a4
Filip Kozłowski <kooziloo@gmail.com>
2014-10-01 13:39:55 +0200

    asdasdasdas ADD trzecie

--- Trzeci/src/Trzeci.java
+++ Trzeci/src/Trzeci.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
-public class Trzeci {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Zadanko {
 
-	}
+private static Scanner odczyt;
+
+static Double a,b;
+
+public static void main(String[] args) {
+System.out.println ("Podaj pierwszš zmiennš: ");
+odczyt = new Scanner(System.in);
+a = odczyt.nextDouble();
+System.out.println ("Podaj druga zmiennš: ");
+b = odczyt.nextDouble();
+System.out.println ("Wynik dodawania: "+(a+b));
+System.out.println ("Wynik odejmowania: "+(a-b));
+System.out.println ("Wynik mno¿enia: "+(a*b));
+System.out.println ("Wynik dzielenia: "+(a/b));
+}
 
 }
