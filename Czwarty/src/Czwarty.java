d3a0dee5f2e28fe0bddae643b606716621122d2e
Filip Kozłowski <kooziloo@gmail.com>
2014-10-01 13:42:29 +0200

    cycki

---
+++ Czwarty/src/Czwarty.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Czwarty {
+	private static Scanner odczyt;
+
+	static Double a,b;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("podaj pierwsz¹ liczbê: ");
+		String liczba1;
+		Scanner odczyt = new Scanner(System.in);
+	}
+
+}
--- Trzeci/src/Trzeci.java
+++ Trzeci/src/Trzeci.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Zadanko {
+public class trzeci {
 
 private static Scanner odczyt;
 
