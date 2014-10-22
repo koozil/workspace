f6af26363a69c1b64ea14a3ed78f213ace4b038b
Filip Kozłowski <kooziloo@gmail.com>
2014-10-22 13:46:17 +0200

    zadanko kolejne z tablic

--- TAB1/src/TAB1.java
+++ TAB1/src/TAB1.java
@@ -10,7 +10,7 @@ tab[i][j] = licznik++;
 
 for(int i=0; i<tab.length; i++){
 	for(int j=0; j<tab[i].length; j++)
-System.out.printf("%3d", tab[i][j]);
+System.out.printf("%30d", tab[i][j]);
 		System.out.println();
 	}
 	}
---
+++ Ztab1/src/Ztab1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ztab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] tab = new int[5];
+		Scanner odczyt = new Scanner(System.in);
+		System.out.println("Podaj elementy tablicy: ");
+		for (int i = 0; i < tab.length; i++) {
+			System.out.println("Element " + i + "; ");
+			tab[i]=odczyt.nextInt();
+		}
+		System.out.println("Oto podana tablica: ");
+		for (int i = 0; i < tab.length; i++)
+			System.out.printf("%4d", tab[i]);
+		int suma = 0;
+		for (int i = 0; i < tab.length; i++)
+			suma += tab[i];
+		System.out.println("Suma wynosi: " + suma);
+
+	}
+}
