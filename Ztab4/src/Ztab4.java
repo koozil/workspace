a907a04300dfc90076a13b426c72f0a4e4f03c39
Filip Kozłowski <kooziloo@gmail.com>
2014-10-27 12:15:57 +0100

    zad 3 i 4

--- Ztab4/src/Ztab4.java
+++ Ztab4/src/Ztab4.java
@@ -3,7 +3,9 @@ public class Ztab4 {
 	public static int i,sumad,sumau;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[][] tab = new int[5][5];
+		
+		}
 	}
 
 }
