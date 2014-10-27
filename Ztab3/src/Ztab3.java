99178ccf685e9f10e4ed56c442d587d25f580c02
Filip Kozłowski <kooziloo@gmail.com>
2014-10-27 12:00:00 +0100

    Zadadnko z tablic #3

---
+++ Ztab3/src/Ztab3.java
@@ -0,0 +1,25 @@
+
+public class Ztab3 {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public static int i,sumap,suman;
+		
+		int[] tab = new int[10];
+		for (int i=0; i < tab.length; i++){
+		tab[i] = (int)(Math.random()*101);
+		}
+		for (int i=0; i < tab.length; i++)
+		System.out.printf ("%4d",tab[i]);
+		for (int i=0; i < tab.length; i++){
+		if (tab[i]%2==0)
+		sumap += tab[i];
+		else
+		suman += tab[i];
+		}
+		System.out.println ("\nSuma parzysta: "+sumap);
+		System.out.println ("\nSuma nieparzysta: "+suman);
+		}
+		}
+
