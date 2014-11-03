853e8679070c9b03332f676cfa664cb1461c8d9c
Filip Kozłowski <kooziloo@gmail.com>
2014-11-03 12:06:50 +0100

    Zadania z tablic ... druga strona

---
+++ Ztab2_1/src/Ztab2_1.java
@@ -0,0 +1,10 @@
+
+public class Ztab2_1 {
+
+	public static void main(String[] args) {
+		int[] tab = new int[20];
+		for (int i = 0; i < tab.length; i++) {
+			tab[i] = 1 + (int) (Math.random() * 30);
+	}
+
+}
